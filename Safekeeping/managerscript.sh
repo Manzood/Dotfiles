b2d11f644cc8f6d0d5cf99c77671f1704ab5a828
Manzood <manzood.naqvi@gmail.com>
2021-01-13 14:15:02 +0530

    Temporary movement of important files

---
+++ Safekeeping/managerscript.sh
@@ -0,0 +1,4 @@
+# this script is going to manage all my dotfiles, and I will add anything I need further
+# over here. Sublime text configuration will soon follow, In all probability. This will make it easy for me 
+# to port my configurations over at a later date
+cp alacritty.yml ~/.config/alacritty/alacritty.yml
