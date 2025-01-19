1f39539e2b88d7996319d3a8a2c4da32f2127ba7
Manzood <manzood.naqvi@gmail.com>
2025-01-19 18:55:58 +0530

    Add rose-pine and format on save

--- Neovim/.config/nvim/init.lua
+++ Neovim/.config/nvim/init.lua
@@ -1,8 +1,17 @@
 require("config.lazy")
 
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 -- vim.opt.clipboard = "unnamedplus" -- I don't think I like this option
 
+vim.keymap.set("i", "jk", "<Esc>")
+vim.keymap.set("i", "kj", "<Esc>")
+vim.keymap.set("i", "JK", "<Esc>")
+vim.keymap.set("i", "Jk", "<Esc>")
+vim.keymap.set("c", "jk", "<Esc>")
+vim.keymap.set("c", "kj", "<Esc>")
+vim.keymap.set("c", "JK", "<Esc>")
+vim.keymap.set("c", "Jk", "<Esc>")
 
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>")
 vim.keymap.set("n", "<space>x", ":.lua<CR>")
@@ -10,7 +19,7 @@ vim.keymap.set("v", "<space>x", ":lua<CR>")
 
 vim.api.nvim_create_autocmd('TextYankPost', {
 	desc = 'Highlight when yanking (copying) text',
-	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', {clear = true}),
+	group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
 	callback = function()
 		vim.highlight.on_yank()
 	end,
--- Neovim/.config/nvim/lua/config/plugins/lsp.lua
+++ Neovim/.config/nvim/lua/config/plugins/lsp.lua
@@ -1,7 +1,42 @@
 return {
-    {
-	"neovim/nvim-lspconfig",
-	config = function()
-	end
-    }
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			{
+				"folke/lazydev.nvim",
+				ft = "lua", -- only load on lua files
+				opts = {
+					library = {
+						-- See the configuration section for more details
+						-- Load luvit types when the `vim.uv` word is found
+						{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+					},
+				},
+			},
+		},
+		config = function()
+			require("lspconfig").lua_ls.setup {}
+			require("lspconfig").clangd.setup {}
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local client = vim.lsp.get_client_by_id(args.data.client_id)
+					if not client then return end
+
+					---@diagnostic disable-next-line: missing-parameter
+					if client.supports_method('textDocument/formatting') then
+						vim.api.nvim_create_autocmd('BufWritePre', {
+							buffer = args.buf,
+							callback = function()
+								vim.lsp.buf.format({ buffer = args.buf, id = client.id })
+							end
+						})
+					end
+				end,
+			})
+
+			vim.keymap.set("n", "<space>ff", function() vim.lsp.buf.format() end)
+			-- vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format({ async = false })]]
+		end,
+	}
 }
