b7c02684eed820fe18968a9340211719c7c39832
Manzood <manzood.naqvi@gmail.com>
2026-04-15 21:23:51 +0530

    Update nvim and tmux config

--- Neovim/.config/nvim/lua/config/plugins/lsp.lua
+++ Neovim/.config/nvim/lua/config/plugins/lsp.lua
@@ -74,14 +74,14 @@ return {
                 -- }
             end
 
-            vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-                border = "rounded",
-            })
-
             vim.api.nvim_create_autocmd('LspAttach', {
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
                     if not client then return end
+
+                    vim.keymap.set("n", "K", function()
+                        vim.lsp.buf.hover({ border = "rounded" })
+                    end, { buffer = args.buf, silent = true })
 
                     ---@diagnostic disable-next-line: missing-parameter
                     if client:supports_method('textDocument/formatting') then
--- Neovim/.config/nvim/lua/config/plugins/mini.lua
+++ Neovim/.config/nvim/lua/config/plugins/mini.lua
@@ -42,7 +42,7 @@ return {
             -- )
             require('mini.icons').setup()
             -- require('mini.pairs').setup()
-            require('mini.ai').setup()
+            -- require('mini.ai').setup()
             -- require('mini.comment').setup()
 
             vim.opt.cursorline = false -- changing it here because mini.basics likes to turn it on by default
